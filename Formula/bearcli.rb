d36c2fb350e95784c88af468f920faebf117960e
Michel Tricot <michel.tricot@gmail.com>
2026-07-28 15:56:23 -0700

    bearcli 1.6.1; formula updated by the bearcli release workflow (no cron)

--- Formula/bearcli.rb
+++ Formula/bearcli.rb
@@ -1,16 +1,14 @@
 class Bearcli < Formula
-  include Language::Python::Virtualenv
-
   desc "The missing CLI for Bear notes"
   homepage "https://michel-tricot.github.io/bearcli/"
-  url "https://files.pythonhosted.org/packages/c1/05/5b9fe7f962be72c55abbb2ef6b0466cf208ca2e73c069fc52c8ac59d57a4/bearcli-1.3.0.tar.gz"
-  sha256 "1d1266c780d47531cde91c4810d4989e7d88c7d5cdfebc20624779f8828a7ad7"
+  url "https://files.pythonhosted.org/packages/47/40/23e99b2db0648ec2e16ad5b0dc12b14b3eee08593c38616fe44b89e840ee/bearcli-1.6.1.tar.gz"
+  sha256 "1e7214ae576ebe4025c47b9cdca60d3614695a4ea2cf442f1c8b207be891522b"
   license "MIT"
 
   depends_on "python@3.13"
 
   def install
-    venv = virtualenv_create(libexec, "python3.13")
+    system Formula["python@3.13"].opt_bin/"python3.13", "-m", "venv", libexec
     system libexec/"bin/pip", "install", "--no-compile", buildpath.to_s
     bin.install_symlink libexec/"bin/bearcli"
   end
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# homebrew-tap
-
-Homebrew formulae for [michel-tricot](https://github.com/michel-tricot)'s tools.
+# Homebrew tap for [`bearcli`](https://github.com/michel-tricot/bearcli)
 
 ```sh
 brew install michel-tricot/tap/bearcli
 ```
+
+The formula is updated automatically by bearcli's release workflow whenever a
+new version is published.
